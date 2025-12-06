3837b486d8b8a635a9bda46a8b4eb47b1936d6eb
Mohammed Aadil Kumshi <kumshiaadil@gmail.com>
2025-12-06 19:33:19 +0530

    Add files via upload

---
+++ blog/schemas.py
@@ -0,0 +1,40 @@
+from pydantic import BaseModel
+from typing import List
+
+
+class Blog(BaseModel):
+    title:str
+    body:str
+    class Config:
+        orm_mode = True
+    
+
+class createUser(BaseModel):
+    name: str
+    email: str
+    password: str
+
+class ShowUser(BaseModel):
+    name: str
+    email: str
+    blogs: List[Blog]
+    class Config:
+        orm_mode = True
+
+class ShowTitle(BaseModel):
+    title: str
+    user_id: ShowUser
+    class Config:
+        orm_mode = True
+
+
+class Login(BaseModel):
+    username: str
+    password: str
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    username: str | None = None
